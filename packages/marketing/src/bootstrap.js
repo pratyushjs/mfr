4a33d3de7f7610d6d8585106ee91edd9b265b112
pratyushjs <pratyush813@gmail.com>
2021-10-10 19:54:15 +0530

    clg added

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -11,7 +11,7 @@ const mount = (el) => {
 // call mount immediately
 if (process.env.NODE_ENV === 'development') {
   const devRoot = document.querySelector('#_marketing-dev-root');
-
+  console.log('running in dev')
   if (devRoot) {
     mount(devRoot);
   }
